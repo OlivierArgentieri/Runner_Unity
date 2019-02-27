8e25e3b8d12db8277ea45f7a9c59ac58424ec124
OlivierArgentieri <olivierargentieri@gmail.com>
2019-02-27 22:23:44 +0100

    [OAI] Begin new level and create test editor script

---
+++ Runner/Assets/Editor/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Jump))][SerializeField]
+public class NewBehaviourScript : Editor
+{
+    private int defautl_index;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var m_tags = UnityEditorInternal.InternalEditorUtility.tags;
+        ((Jump)target).m_tags = m_tags;
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Tags");
+        
+        ((Jump)target).m_tag_index = EditorGUILayout.IntField(EditorGUILayout.Popup(((Jump)target).m_tag_index, m_tags));
+
+
+        GUILayout.EndHorizontal();
+    }
+}
--- Runner/Assets/Scripts/Player/Components/Jump.cs
+++ Runner/Assets/Scripts/Player/Components/Jump.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Jump : MonoBehaviour
 {
-    //public string[] m_tags = UnityEditorInternal.InternalEditorUtility.tags;
     [SerializeField] private float m_jump_power;
-    [SerializeField] private bool m_only_on_platform;
+
+    [Header("Gravity")]
     [SerializeField] private bool m_mass_as_gravity_;
 
     private Rigidbody m_rigidbody_;
     private bool m_is_jumping_;
 
+
+    [Header("Constraint")]
+    [SerializeField] private bool m_only_on_gameobject;
+    
     private void Start()
     {
         
@@ -20,16 +24,16 @@ public class Jump : MonoBehaviour
 
     public void MakeJump()
     {
-        if (m_only_on_platform && !m_is_jumping_)
+        if (m_only_on_gameobject && !m_is_jumping_)
             m_rigidbody_.AddForce(Vector3.up * m_jump_power);
 
-        if (!m_only_on_platform)
+        if (!m_only_on_gameobject)
             m_rigidbody_.AddForce(Vector3.up * m_jump_power);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Platform")
+        if (collision.gameObject.tag =="Platform")
             this.m_is_jumping_ = false;
     }
 
