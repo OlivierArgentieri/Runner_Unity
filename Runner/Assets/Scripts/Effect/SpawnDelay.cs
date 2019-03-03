5add9b70cfc111b4b35af72186300f96f8b7a6a4
OlivierArgentieri <olivierargentieri@gmail.com>
2019-03-03 22:44:08 +0100

    [OAI] Update lvl3

---
+++ Runner/Assets/Scripts/Effect/SpawnDelay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDelay : MonoBehaviour
+{
+    [SerializeField] private GameObject m_prefabs;
+    [SerializeField] private float m_cooldown;
+
+    private float m_time_count_;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > m_time_count_)
+        {
+            Create();
+            ResetCount();
+        }
+    }
+
+    private void Create()
+    {
+        Instantiate(m_prefabs, transform.position, Quaternion.identity);
+    }
+
+    private void ResetCount()
+    {
+        m_time_count_ = Time.time + m_cooldown;
+    }
+}
---
+++ Runner/Assets/Scripts/Platform/MovePlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    [SerializeField] private float m_speed;
+    [SerializeField] private Vector3 m_direction_vector;
+    private bool m_reversed_;
+
+    private void Awake()
+    {
+        m_reversed_ = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(m_speed);
+    }
+
+    private void Move(float _fSpeed)
+    {
+        if (m_reversed_)
+            transform.position += -m_direction_vector * Time.deltaTime * _fSpeed;
+
+        else
+            transform.position += m_direction_vector * Time.deltaTime * _fSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player")
+            m_reversed_ = !m_reversed_;
+        else
+            collision.gameObject.transform.parent = transform;
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            collision.gameObject.transform.parent = null;
+    }
+}
