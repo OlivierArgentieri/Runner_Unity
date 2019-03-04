1471616fdd29b190acd05fc5ea39443bea553ceb
OlivierArgentieri <30509073+OlivierArgentieri@users.noreply.github.com>
2019-03-04 13:55:08 +0100

    [OAI] complete method

--- Runner/Assets/Scripts/SaveManager.cs
+++ Runner/Assets/Scripts/SaveManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
 
 public class SaveManager : MonoBehaviour
 {
     public static SaveManager m_instance;
-    
+
+    private string m_path_;
     private void Awake()
     {
         if (m_instance == null)
@@ -20,7 +23,7 @@ public class SaveManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+      m_path_ = Application.streamingAssetsPath + "/save.json";
     }
 
     // Update is called once per frame
@@ -29,8 +32,26 @@ public class SaveManager : MonoBehaviour
 
     }
 
-    public void Save(Player _pPlayerObject )
+    public void SaveTime(float _fTime)
     {
-
+        SaveData save = new SaveData();
+        save.m_saved_time = _fTime;
+        File.WriteAllText(m_path_, JsonUtility.ToJson(save));
     }
+
+    public float GetSavedTime(float _fTime)
+    {
+        string json = File.ReadAllText(m_path_);
+        SaveData save = JsonUtility.FromJson<SaveData>(m_path_);
+
+        Debug.Log(save.m_saved_time);
+        return 0;
+    }
+
+    
+}
+
+public class SaveData
+{
+    public float m_saved_time;
 }
