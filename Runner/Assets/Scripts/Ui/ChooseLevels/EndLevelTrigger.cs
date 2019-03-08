e824c98bcc6bf71ebfbfe04a072896ba891f9904
OlivierArgentieri <30509073+OlivierArgentieri@users.noreply.github.com>
2019-03-08 16:58:31 +0100

    [OAI] Begin Menu

---
+++ Runner/Assets/Scripts/Ui/ChooseLevels/EndLevelTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLEvel : MonoBehaviour
+{
+    [SerializeField] private string m_name_scene;
+    
+    public void LoadScene()
+    {
+        LevelManager.GetInstance().SwitchScene(m_name_scene);
+
+    }
+}
