9184f14dfa61d3ffac178ce1f27694c248a3fdd3
OlivierArgentieri <30509073+OlivierArgentieri@users.noreply.github.com>
2019-03-11 11:11:22 +0100

    [OAI] Clean MainCanvasScript

--- Runner/Assets/Scripts/Ui/MainCanvas/MainCanvasScript.cs
+++ Runner/Assets/Scripts/Ui/MainCanvas/MainCanvasScript.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class MainCanvasScript : MonoBehaviour
 {
-    // Use this for initialization
+    
     void Start()
     {
-        //DontDestroyOnLoad(this.gameObject);
+        
     }
 }
